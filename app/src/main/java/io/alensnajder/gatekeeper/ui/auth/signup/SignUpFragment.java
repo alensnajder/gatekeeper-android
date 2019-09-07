c0e3fd1997626ca1ef5f77cd0477466d9650d839
Alen Snajder <alen.snajder@gmail.com>
2019-09-07 14:21:02 +0200

    Add sign up fragment design

--- app/src/main/java/io/alensnajder/gatekeeper/ui/auth/signup/SignUpFragment.java
+++ app/src/main/java/io/alensnajder/gatekeeper/ui/auth/signup/SignUpFragment.java
@@ -1,24 +1,22 @@
 package io.alensnajder.gatekeeper.ui.auth.signup;
 
-import androidx.lifecycle.ViewModelProvider;
-import androidx.lifecycle.ViewModelProviders;
-
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.lifecycle.ViewModelProviders;
 
 import javax.inject.Inject;
 
 import dagger.android.support.DaggerFragment;
 import io.alensnajder.gatekeeper.R;
 
-public class SignUpFragment extends DaggerFragment {
+public class SignUpFragment extends DaggerFragment implements View.OnClickListener {
 
     @Inject
     ViewModelProvider.Factory viewModelFactory;
@@ -31,7 +29,11 @@ public class SignUpFragment extends DaggerFragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_sign_up, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_sign_up, container, false);
+        Button btSignUp = rootView.findViewById(R.id.btSignUp);
+        btSignUp.setOnClickListener(this);
+
+        return rootView;
     }
 
     @Override
@@ -40,4 +42,11 @@ public class SignUpFragment extends DaggerFragment {
         signUpViewModel = ViewModelProviders.of(this, viewModelFactory).get(SignUpViewModel.class);
     }
 
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btSignUp:
+                break;
+        }
+    }
 }
