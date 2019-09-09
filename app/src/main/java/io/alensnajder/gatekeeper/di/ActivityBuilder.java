98f2add66056879f69713329e3c424f4f2bf1167
Alen Snajder <alen.snajder@gmail.com>
2019-09-09 22:09:43 +0200

    Add main activity

--- app/src/main/java/io/alensnajder/gatekeeper/di/ActivityBuilder.java
+++ app/src/main/java/io/alensnajder/gatekeeper/di/ActivityBuilder.java
@@ -4,9 +4,14 @@ import dagger.Module;
 import dagger.android.ContributesAndroidInjector;
 import io.alensnajder.gatekeeper.ui.auth.AuthActivity;
 import io.alensnajder.gatekeeper.ui.auth.AuthFragmentProvider;
+import io.alensnajder.gatekeeper.ui.main.MainActivity;
+import io.alensnajder.gatekeeper.ui.main.MainFragmentProvider;
 
 @Module
 public abstract class ActivityBuilder {
     @ContributesAndroidInjector(modules = AuthFragmentProvider.class)
     abstract AuthActivity bindAuthActivity();
+
+    @ContributesAndroidInjector(modules = MainFragmentProvider.class)
+    abstract MainActivity bindMainActivity();
 }
---
+++ app/src/main/java/io/alensnajder/gatekeeper/ui/main/MainFragmentProvider.java
@@ -0,0 +1,8 @@
+package io.alensnajder.gatekeeper.ui.main;
+
+import dagger.Module;
+
+@Module
+public abstract class MainFragmentProvider {
+
+}
