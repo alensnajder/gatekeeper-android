7c9773a29275dfe1b78c71a6aadc7863c5f05938
Alen Snajder <alen.snajder@gmail.com>
2019-10-22 01:09:59 +0200

    Add progress bar to user details

--- app/src/main/java/io/alensnajder/gatekeeper/ui/user/detail/UserDetailFragment.java
+++ app/src/main/java/io/alensnajder/gatekeeper/ui/user/detail/UserDetailFragment.java
@@ -11,10 +11,12 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.lifecycle.ViewModelProviders;
@@ -35,6 +37,8 @@ public class UserDetailFragment extends DaggerFragment implements View.OnClickLi
     private TextView tvEmail;
     private TextView tvStatus;
     private TextView tvRole;
+    private ProgressBar progressBar;
+    private ConstraintLayout content;
 
     private User user;
 
@@ -51,6 +55,8 @@ public class UserDetailFragment extends DaggerFragment implements View.OnClickLi
                              @Nullable Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_user_detail, container, false);
         setHasOptionsMenu(true);
+        progressBar = rootView.findViewById(R.id.progress_bar);
+        content = rootView.findViewById(R.id.content);
         tvFullName = rootView.findViewById(R.id.tvFullName);
         tvEmail = rootView.findViewById(R.id.tvEmail);
         tvStatus = rootView.findViewById(R.id.tvStatus);
@@ -64,23 +70,32 @@ public class UserDetailFragment extends DaggerFragment implements View.OnClickLi
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-
-        UserDetailFragmentArgs args = UserDetailFragmentArgs.fromBundle(getArguments());
         userDetailViewModel = ViewModelProviders.of(this, viewModelFactory).get(UserDetailViewModel.class);
-        userDetailViewModel.fetchUser(args.getUserId());
+
+        fetchUser();
         onUser();
+    }
+
+    private void fetchUser() {
+        content.setVisibility(View.GONE);
+        progressBar.setVisibility(View.VISIBLE);
+        UserDetailFragmentArgs args = UserDetailFragmentArgs.fromBundle(getArguments());
+        userDetailViewModel.fetchUser(args.getUserId());
     }
 
     private void onUser() {
         userDetailViewModel.getUserLive().observe(this, new Observer<LiveHolder>() {
             @Override
             public void onChanged(LiveHolder userHolder) {
+                progressBar.setVisibility(View.GONE);
+                content.setVisibility(View.VISIBLE);
                 switch (userHolder.status) {
                     case SUCCESS:
                         user = (User) userHolder.data;
                         tvFullName.setText(user.getFullName());
                         tvEmail.setText(user.getEmail());
-                        //tvStatus.setText((user.isActive()) ? "Active" : "Inactive");
+                        tvStatus.setText((user.isActive()) ? "Active" : "Inactive");
+                        tvRole.setText((user.isAdmin()) ? "Admin" : "User");
                         break;
                     case ERROR:
                         Snackbar.make(getView(), userHolder.errorMessage, Snackbar.LENGTH_LONG).show();
